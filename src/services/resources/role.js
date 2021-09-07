7c0faf95015a9420062dd986e31c3beca1f60008
Saad <saad.mahmood@gems.techverx.com>
2021-09-07 15:45:28 +0500

    add  department permission in admin role

--- src/services/resources/role.js
+++ src/services/resources/role.js
@@ -11,6 +11,7 @@ class RoleService extends AccountResourceService {
                 permissions: {
                     Roles: ['*'],
                     Users: ['*'],
+                    Departments: ['*'],
                 },
                 default: true,
             },
