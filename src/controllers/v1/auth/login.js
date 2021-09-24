ad73aca8b548a7ea5f20db4b31331f979c01c0bc
Faiz Ali <faizali.virgo@gmail.com>
2021-09-24 18:21:58 +0500

    add user object in login res

--- src/controllers/v1/auth/login.js
+++ src/controllers/v1/auth/login.js
@@ -19,7 +19,9 @@ const login = async (req, res, next) => {
                 }
                 const jwtToken = jwt.sign(decodeObj, config.jwt.secret, { expiresIn: '2h' })
 
-                res.send({ message: 'Welcome', token: jwtToken, permissions: role.permissions })
+                res.send({
+                    message: 'Welcome', token: jwtToken, permissions: role.permissions, user,
+                })
             } else {
                 next(new Error('Password do not match'))
             }
