43c9529a5823d4f2cc048de4a4a4855f3a3e0c9c
Faiz Ali <faizali.virgo@gmail.com>
2021-09-27 12:13:34 +0500

    Task 1-change the logged-in user password

--- src/controllers/v1/auth/index.js
+++ src/controllers/v1/auth/index.js
@@ -2,7 +2,8 @@ const login = require('./login')
 const forgetPassword = require('./forgetPassword')
 const resetPassword = require('./resetPassword')
 const me = require('./me')
+const changepassword = require('./changepassword')
 
 module.exports = {
-    login, forgetPassword, resetPassword, me,
+    login, forgetPassword, resetPassword, me, changepassword,
 }
