bd2a971ca2ef7b8c42516b944c6235130e881712
Faiz Ali <faizali.virgo@gmail.com>
2021-11-04 13:14:03 +0500

    TaskList

--- src/migrations/20211104072222-create-task-list.js
+++ src/migrations/20211104072222-create-task-list.js
@@ -1,50 +1,49 @@
-'use strict';
 module.exports = {
-  up: async (queryInterface, Sequelize) => {
-    await queryInterface.createTable('taskLists', {
-      id: {
-        allowNull: false,
-        primaryKey: true,
-        type: Sequelize.UUID,
-        defaultValue: Sequelize.UUIDv4,
-      },
-      title: {
-        type: Sequelize.STRING
-      },
-      author: {
-        type: Sequelize.STRING
-      },
-      description: {
-        type: Sequelize.TEXT
-      },
-      links: {
-        type: Sequelize.STRING
-      },
-      type: {
-        type: Sequelize.STRING
-      },
-      forTemplate: {
-        type: Sequelize.BOOLEAN
-      },
-      active: {
-        type: Sequelize.BOOLEAN,
-        defaultValue: true,
-      },
-      deletedAt: {
-        allowNull: true,
-        type: Sequelize.DATE,
-      },
-      createdAt: {
-        allowNull: false,
-        type: Sequelize.DATE
-      },
-      updatedAt: {
-        allowNull: false,
-        type: Sequelize.DATE
-      }
-    });
-  },
-  down: async (queryInterface) => {
-    await queryInterface.dropTable('taskLists');
-  }
-};
+    up: async (queryInterface, Sequelize) => {
+        await queryInterface.createTable('taskLists', {
+            id: {
+                allowNull: false,
+                primaryKey: true,
+                type: Sequelize.UUID,
+                defaultValue: Sequelize.UUIDv4,
+            },
+            title: {
+                type: Sequelize.STRING,
+            },
+            author: {
+                type: Sequelize.STRING,
+            },
+            description: {
+                type: Sequelize.TEXT,
+            },
+            links: {
+                type: Sequelize.STRING,
+            },
+            type: {
+                type: Sequelize.STRING,
+            },
+            forTemplate: {
+                type: Sequelize.BOOLEAN,
+            },
+            active: {
+                type: Sequelize.BOOLEAN,
+                defaultValue: true,
+            },
+            deletedAt: {
+                allowNull: true,
+                type: Sequelize.DATE,
+            },
+            createdAt: {
+                allowNull: false,
+                type: Sequelize.DATE,
+            },
+            updatedAt: {
+                allowNull: false,
+                type: Sequelize.DATE,
+            },
+        })
+    },
+    down: async (queryInterface) => {
+        await queryInterface.dropTable('taskLists')
+    },
+}
--- src/models/tasklist.js
+++ src/models/tasklist.js
@@ -1,62 +1,61 @@
-'use strict';
 const {
-  Model
-} = require('sequelize');
+    Model,
+} = require('sequelize')
 const sequelizePaginate = require('sequelize-paginate')
-module.exports = (sequelize, DataTypes) => {
-  class TaskList extends Model {
 
-    static associate() {
-      // define association here
+module.exports = (sequelize, DataTypes) => {
+    class TaskList extends Model {
+        static associate() {
+            // define association here
+        }
     }
-  };
-  TaskList.init({
-    id: {
-      type: DataTypes.UUID,
-      defaultValue: DataTypes.UUIDV4,
-      primaryKey: true,
-    },
-    title: {
-      type: DataTypes.STRING,
-    },
-    author: {
-      type: DataTypes.STRING,
-    },
-    links: {
-      type: DataTypes.STRING,
-    },
-    type: {
-      type: DataTypes.STRING,
-    },
-    description: {
-      type: DataTypes.TEXT,
-    },
-    forTemplate: {
-      type: DataTypes.BOOLEAN,
-      defaultValue: true,
-    },
-    active: {
-      type: DataTypes.BOOLEAN,
-      defaultValue: true,
-    },
-    createdAt: {
-      allowNull: false,
-      type: DataTypes.DATE,
-    },
-    updatedAt: {
-      allowNull: true,
-      type: DataTypes.DATE,
-    },
-    deletedAt: {
-      allowNull: true,
-      type: DataTypes.DATE,
-    },
-  }, {
-    sequelize,
-    modelName: 'TaskList',
-    tableName: 'taskLists',
-    paranoid: true,
-  });
-  sequelizePaginate.paginate(TaskList)
-  return TaskList;
-};
+    TaskList.init({
+        id: {
+            type: DataTypes.UUID,
+            defaultValue: DataTypes.UUIDV4,
+            primaryKey: true,
+        },
+        title: {
+            type: DataTypes.STRING,
+        },
+        author: {
+            type: DataTypes.STRING,
+        },
+        links: {
+            type: DataTypes.STRING,
+        },
+        type: {
+            type: DataTypes.STRING,
+        },
+        description: {
+            type: DataTypes.TEXT,
+        },
+        forTemplate: {
+            type: DataTypes.BOOLEAN,
+            defaultValue: true,
+        },
+        active: {
+            type: DataTypes.BOOLEAN,
+            defaultValue: true,
+        },
+        createdAt: {
+            allowNull: false,
+            type: DataTypes.DATE,
+        },
+        updatedAt: {
+            allowNull: true,
+            type: DataTypes.DATE,
+        },
+        deletedAt: {
+            allowNull: true,
+            type: DataTypes.DATE,
+        },
+    }, {
+        sequelize,
+        modelName: 'TaskList',
+        tableName: 'taskLists',
+        paranoid: true,
+    })
+    sequelizePaginate.paginate(TaskList)
+    return TaskList
+}
